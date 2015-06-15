aee70c9f6e7f7be99cfcdc48ce31417ab7d01b75
Stefan B <Stefan.bie@live.de>
2015-06-15 10:57:18 +0200

    - remove unused method - add print method

--- _lib/utils.cpp
+++ _lib/utils.cpp
@@ -7,30 +7,63 @@
 #include <GLFW/glfw3.h>
 #include <GLUT/glut.h>
 #include <iostream>
-
-
+//this creates my translation matrix which causes the cube to disappear
+using namespace std;
 void Utils::setGlobalCoords(Vec3 &local, Vec3 &global) {
     GLint viewport[4];
     GLdouble modelview[16];
-    GLdouble viewVector[3];
-    GLdouble projection[16];
+    GLdouble proj[16];
+    GLdouble inverse[16];
 
-    //get the matrices
-    glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
-    GLdouble globalX, globalY, globalZ;
-    viewVector[0] = modelview[8];
-    viewVector[1] = modelview[9];
-    viewVector[2] = modelview[10];
+    long double  tx  = local.p[0];
+    long double  ty  = local.p[1];
+    long double  tz  = local.p[2];
+    long double  tw  = 1;
 
-    glGetDoublev(GL_PROJECTION_MATRIX, projection);
-    glGetIntegerv(GL_VIEWPORT, viewport);
+    long double newX;
+    long double newY;
+    long double newZ;
+    long double newW;
+    glGetDoublev( GL_MODELVIEW_MATRIX, modelview );
+    glGetDoublev( GL_PROJECTION_MATRIX, proj );
+    newX=modelview[0]*tx    +modelview[4]*ty    +modelview[8]*ty    +modelview[12] * tw;
+    newY=modelview[1]*tx    +modelview[5]*ty    +modelview[9]*ty    +modelview[13] * tw;
+    newZ=modelview[2]*tx    +modelview[6]*ty    +modelview[10]*ty   +modelview[14] * tw;
+    newW=modelview[3]*tx    +modelview[7]*ty    +modelview[11]*ty   +modelview[15] * tw;
 
-    int res = gluProject(local.p[0], local.p[1], local.p[2], modelview, projection, viewport, &globalX, &globalY,
-                         &globalZ);
-    global.p[0] = globalX;
-    global.p[1] = globalY;
-    global.p[2] = globalZ;
+    /*
+     *  modelViewMatric = [ 1 0 0 0
+     *                      0 1 0 0
+     *                      0 0 1 0
+     *                      0 0 0 1
+     *                      ]
+     *
+     *
+     * */
 
+
+    //Utils::logMatrix(modelview);
+    //Utils::logMatrix(proj);
+
+    global.p[0] = newX;
+    global.p[1] = newY;
+    global.p[2] = newZ;
+
+
+}
+void Utils::logMatrix(double m[16]){
+    std::cout << m[0] << "\t " << m[4] << "\t " << m[8]<< "\t " << m[12]<< std::endl;
+    std::cout << m[1] << "\t " << m[5] << "\t " << m[9]<< "\t " << m[13]<< std::endl;
+    std::cout << m[2] << "\t " << m[6] << "\t " << m[10]<< "\t " << m[14]<< std::endl;
+    std::cout << m[3] << "\t " << m[7] << "\t " << m[11]<< "\t " << m[15]<< std::endl;
+
+}
+void Utils::printVec3(Vec3 &point) {
+    cout << "X:"<<point.p[0]   << "\t Y: " << point.p[1] << " \t Z:" << point.p[2]<< endl;
+}
+
+void Utils::printVec3(Vec3 &point, string desc) {
+    cout  << "Point: " << desc << "\t X:"<<point.p[0]   << "\t Y: " << point.p[1] << " \t Z:" << point.p[2]<< endl;
 }
 void Utils::drawAxis(Vec3 &point, double length) {
 
--- _lib/utils.h
+++ _lib/utils.h
@@ -6,11 +6,15 @@
 #define GLFW3_DEMO_WITH_CMAKE_UTILS_H
 
 #include  "vec3.hpp"
-
+#include <iostream>
+using  namespace std;
 class Utils {
 
 public:
     static void setGlobalCoords(Vec3 &local, Vec3 &global);
+    static void logMatrix(double m[16]);
     static void drawAxis(Vec3 &point, double length);
+    static void printVec3(Vec3 &point);
+    static void printVec3(Vec3 &point, string desc);
 };
 #endif //GLFW3_DEMO_WITH_CMAKE_UTILS_H
